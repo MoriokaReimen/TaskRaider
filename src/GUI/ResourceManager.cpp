bf28b7ed69d369f403462baddee61a8a85c56a02
Kei@Glou-Glout <menschenjager.mark.neun@gmail.com>
2020-04-11 19:32:08 +0900

    [update] add text loading function to ResourceManager

--- src/GUI/ResourceManager.cpp
+++ src/GUI/ResourceManager.cpp
@@ -1,5 +1,6 @@
 #include <ResourceManager.hpp>
 #include <exception>
+#include <fstream>
 #include <sstream>
 
 namespace GUI
@@ -40,6 +41,23 @@ void ResourceManager::loadTexture(const std::string &path, const std::string &na
     }
 }
 
+void ResourceManager::loadText(const std::string &path, const std::string &name)
+{
+    std::ifstream file(path);
+    std::string text;
+    if (file.is_open())
+    {
+        std::getline(file, text, char(-1));
+        texts_[name] = text;
+    }
+    else
+    {
+        std::stringstream ss;
+        ss << "Failed to load Text: " << path;
+        throw std::runtime_error(ss.str());
+    }
+}
+
 void ResourceManager::freeFont(const std::string &name)
 {
     fonts_.erase(name);
@@ -48,6 +66,11 @@ void ResourceManager::freeFont(const std::string &name)
 void ResourceManager::freeTexture(const std::string &name)
 {
     textures_.erase(name);
+}
+
+void ResourceManager::freeText(const std::string &name)
+{
+    texts_.erase(name);
 }
 
 void ResourceManager::freeFont()
@@ -60,10 +83,16 @@ void ResourceManager::freeTexture()
     textures_.clear();
 }
 
+void ResourceManager::freeText()
+{
+    texts_.clear();
+}
+
 void ResourceManager::free()
 {
     freeTexture();
     freeFont();
+    freeText();
 }
 
 sf::Font &ResourceManager::getFont(const std::string &name)
@@ -74,6 +103,11 @@ sf::Font &ResourceManager::getFont(const std::string &name)
 sf::Texture &ResourceManager::getTexture(const std::string &name)
 {
     return textures_[name];
+}
+
+std::string &ResourceManager::getText(const std::string &name)
+{
+    return texts_[name];
 }
 
 }; // namespace GUI
--- src/GUI/ResourceManager.hpp
+++ src/GUI/ResourceManager.hpp
@@ -9,18 +9,23 @@ class ResourceManager
 {
     std::unordered_map<std::string, sf::Font> fonts_;
     std::unordered_map<std::string, sf::Texture> textures_;
+    std::unordered_map<std::string, std::string> texts_;
 
 public:
     ResourceManager();
     void loadFont(const std::string &path, const std::string &name);
     void loadTexture(const std::string &path, const std::string &name);
+    void loadText(const std::string &path, const std::string &name);
     void freeFont(const std::string &name);
     void freeTexture(const std::string &name);
+    void freeText(const std::string &name);
     void freeFont();
     void freeTexture();
+    void freeText();
     void free();
     sf::Font &getFont(const std::string &name);
     sf::Texture &getTexture(const std::string &name);
+    std::string &getText(const std::string &name);
 };
 
 inline ResourceManager resource;
